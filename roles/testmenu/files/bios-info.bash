8ca3a57c9757cda00bbc33fa4cb774f543e9b5f8
Andreas <asc4asc@users.noreply.github.com>
2025-02-24 08:37:00 +0100

    Update bios-info.bash

--- roles/testmenu/files/bios-info.bash
+++ roles/testmenu/files/bios-info.bash
@@ -4,4 +4,10 @@ sudo dmidecode -t bios -t system -t baseboard
 
 echo -n "Name: "; sudo dmidecode -s baseboard-product-name
 echo -n "Serial Number: "; sudo dmidecode -s baseboard-serial-number
+
+echo -n "Baseboard Manufacturer: "; sudo dmidecode -s baseboard-manufacturer
+echo -n "Baseboard Version (Rev): "; sudo dmidecode -s baseboard-version
+
 echo -n "BIOS Build: "; sudo dmidecode -s bios-version
+echo -n "BIOS Date: "; sudo dmidecode -s bios-release-date
+
