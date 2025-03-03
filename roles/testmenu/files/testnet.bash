31ec88395ae2b2cb790da7dca024e53854b4b1f5
Andreas <asc4asc@users.noreply.github.com>
2025-03-03 15:51:53 +0100

    Update testnet.bash

--- roles/testmenu/files/testnet.bash
+++ roles/testmenu/files/testnet.bash
@@ -1,41 +1,45 @@
 #!/bin/bash
 
 # Variables
-INTERFACE_SERVER="eth0"
-INTERFACE_CLIENT="eth1"
+INTERFACE_SERVER="enp3s0"
+INTERFACE_CLIENT="enp4s0"
 IP_SERVER="192.168.1.1"
 IP_CLIENT="192.168.1.2"
 DURATION=60
 PARALLEL_STREAMS=4
 INTERVAL=1
 
-# Install necessary packages
-sudo apt-get update
-sudo apt-get install -y iperf3 linux-tools-common linux-tools-$(uname -r)
+# Create network namespaces
+sudo ip netns add server_ns
+sudo ip netns add client_ns
 
-# Set CPU C-states (disable all except C0)
-sudo cpupower idle-set -D 0
+# Move interfaces to namespaces
+sudo ip link set $INTERFACE_SERVER netns server_ns
+sudo ip link set $INTERFACE_CLIENT netns client_ns
 
-# Assign IP addresses to interfaces
-sudo ip addr add $IP_SERVER/24 dev $INTERFACE_SERVER
-sudo ip addr add $IP_CLIENT/24 dev $INTERFACE_CLIENT
+# Assign IP addresses
+sudo ip netns exec server_ns ip addr add $IP_SERVER/24 dev $INTERFACE_SERVER
+sudo ip netns exec client_ns ip addr add $IP_CLIENT/24 dev $INTERFACE_CLIENT
 
-# Bring up the interfaces
-sudo ip link set $INTERFACE_SERVER up
-sudo ip link set $INTERFACE_CLIENT up
+# Bring up loopback interfaces
+sudo ip netns exec server_ns ip link set lo up
+sudo ip netns exec client_ns ip link set lo up
 
-# Start iperf3 server on the server interface
-iperf3 -s -B $IP_SERVER &
+# Bring up network interfaces
+sudo ip netns exec server_ns ip link set $INTERFACE_SERVER up
+sudo ip netns exec client_ns ip link set $INTERFACE_CLIENT up
+
+# Start iperf3 server in server namespace
+sudo ip netns exec server_ns iperf3 -s -B $IP_SERVER &
 
 # Wait a moment to ensure the server starts
 sleep 2
 
-# Run iperf3 client on the client interface
-iperf3 -c $IP_SERVER -B $IP_CLIENT -t $DURATION -P $PARALLEL_STREAMS -i $INTERVAL
+# Run iperf3 client in client namespace
+sudo ip netns exec client_ns iperf3 -c $IP_SERVER -B $IP_CLIENT -t $DURATION -P $PARALLEL_STREAMS -i $INTERVAL
 
-# Re-enable all C-states after the test
-sudo cpupower idle-set -E
-
-# Clean up IP addresses
-sudo ip addr del $IP_SERVER/24 dev $INTERFACE_SERVER
-sudo ip addr del $IP_CLIENT/24 dev $INTERFACE_CLIENT
+# Clean up
+sudo ip netns exec server_ns ip link set $INTERFACE_SERVER netns 1
+sudo ip netns exec client_ns ip link set $INTERFACE_CLIENT netns 1
+sudo ip netns delete server_ns
+sudo ip netns delete client_ns
