afc3ad2f3e99a1aec2a4bdf4bb9dfe6893911354
Andreas <asc4asc@users.noreply.github.com>
2025-03-27 14:37:01 +0100

    Update ringbuf.py

--- roles/testmenu/files/ringbuf.py
+++ roles/testmenu/files/ringbuf.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 
-class RingBuffer:def init(self, size):
-self.size = size
-self.buffer = []
-self.index = 0
+class RingBuffer:
+    def __init__(self, size):
+        self.size = size
+        self.buffer = []
+        self.index = 0
 
-def add(self, number):
-if number in self.buffer:
-return False  # Nummer existiert bereits im Puffer
-if len(self.buffer) < self.size:
-self.buffer.append(number)
-else:
-self.buffer[self.index] = number
-self.index = (self.index + 1) % self.size
-return True
+    def add(self, number):
+        if number in self.buffer:
+            return False  # Nummer existiert bereits im Puffer
+        if len(self.buffer) < self.size:
+            self.buffer.append(number)
+        else:
+            self.buffer[self.index] = number
+        self.index = (self.index + 1) % self.size
+        return True
 
-def str(self):
-return str(self.buffer)
+    def __str__(self):
+        return str(self.buffer)
 
-Erstellen eines Ringpuffers mit einer maximalen Größe von 100
+# Erstellen eines Ringpuffers mit einer maximalen Größe von 100
 ring_buffer = RingBuffer(100)
 
-Beispielhafte Nutzung
+# Beispielhafte Nutzung
 numbers_to_add = [123456, 234567, 345678, 456789, 567890, 678901, 789012, 890123, 901234, 123456]
 
 for number in numbers_to_add:
-if ring_buffer.add(number):
-print(f"Nummer {number} wurde dem Puffer hinzugefügt.")
-else:
-print(f"Nummer {number} existiert bereits im Puffer.")
+    if ring_buffer.add(number):
+        print(f"Nummer {number} wurde dem Puffer hinzugefügt.")
+    else:
+        print(f"Nummer {number} existiert bereits im Puffer.")
 
 print("Endgültiger Pufferzustand:", ring_buffer)
