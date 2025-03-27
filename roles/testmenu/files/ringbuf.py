eb5935041d8ee1dd739c675a8efa65ff730b5194
Andreas <asc4asc@users.noreply.github.com>
2025-03-27 14:58:19 +0100

    Update ringbuf.py

--- roles/testmenu/files/ringbuf.py
+++ roles/testmenu/files/ringbuf.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
+import os
+import pickle
+
 class RingBuffer:
-    def __init__(self, size):
+    def __init__(self, size, filename):
         self.size = size
         self.buffer = []
         self.index = 0
+        self.filename = filename
+        self.load_buffer()
 
     def add(self, number):
         if number in self.buffer:
@@ -14,16 +19,26 @@ class RingBuffer:
         else:
             self.buffer[self.index] = number
         self.index = (self.index + 1) % self.size
+        self.save_buffer()
         return True
+
+    def save_buffer(self):
+        with open(self.filename, 'wb') as f:
+            pickle.dump((self.buffer, self.index), f)
+
+    def load_buffer(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'rb') as f:
+                self.buffer, self.index = pickle.load(f)
 
     def __str__(self):
         return str(self.buffer)
 
-# Erstellen eines Ringpuffers mit einer maximalen Größe von 100
-ring_buffer = RingBuffer(100)
+# Erstellen eines Ringpuffers mit einer maximalen Größe von 100 und einer Datei zum Speichern des Zustands
+ring_buffer = RingBuffer(100, 'ring_buffer.pkl')
 
 # Beispielhafte Nutzung
-numbers_to_add = [123456, 234567, 345678, 456789, 567890, 678901, 789012, 890123, 901234, 123456]
+numbers_to_add = [12345678, 23456789, 34567890, 45678901, 56789012, 67890123, 78901234, 89012345, 90123456, 12345678]
 
 for number in numbers_to_add:
     if ring_buffer.add(number):
